95bfd682665bcfe359c5dddc4e75499436a64dd9
ErriekaP <yen.palanca@gmail.com>
2023-10-05 19:07:02 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-
+//import { useState } from 'react'
 import './App.css'
+import Post from './components/Post'
 
 function App() {
 
-
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+  
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+      <Post></Post>
+      <div className='flex items-center gap-3 mt-3'>
+      {Array.from({length: 5 }).map((value, index) => {
+        return(
+          // eslint-disable-next-line react/jsx-key
+          <div className='p-2 border font-bold hover:cursor-pointer bg-red-950 text-white'>
+            {index + 1}
+          </div> 
+        )
+      })}
+      </div>
+    </div>
+  
+
+   
   )
 }
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+//import React from 'react'
+
+const Post = () => {
+  return (
+    <div className=" text-center p-4 bg-white border border-gray-400/30 w-[250px] border-double border-8 border-red-900">
+        <h4>Title</h4>
+        <p>Body</p>
+    </div>
+  )
+}
+
+export default Post
